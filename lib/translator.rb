d20df7959629747a706c45db5d5b04ce27b6a758
Daniel Mayle <dmayle012@gmail.com>
2020-01-11 20:11:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,15 +1,14 @@
-# require modules here
 require "yaml"
 
 def load_library(file_path)
-  load_emoticons = YAML.load_file(file_path)
+  emoticons = YAML.load_file(file_path)
   
   new_hash = {
     'get_meaning' => {},
     'get_emoticon' => {}
   }
 
-  load_emoticons.each do |key,value|
+  emoticons.each do |key,value|
     new_hash['get_emoticon'][value[0]] = emoticons[key][1]
     new_hash['get_meaning'][value[1]] = key
   end
